59863900708305377ca9ab5092d8226628acd389
Seoulful <79722803+Seoulful@users.noreply.github.com>
2022-05-11 15:55:50 -0700

    Added While_Statement_Assignment

---
+++ While_Statement_Assignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace While_Statement_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input your name.");
+            string name = Console.ReadLine();
+            bool authorizedUser = name == "Glenn";
+
+            //while (!authorizedUser)
+            //{
+            //    switch (name)
+            //    {
+            //        default:
+            //            Console.WriteLine("You are not an authorized user.");
+            //            Console.WriteLine("Please input a different name.");
+            //            name = Console.ReadLine();
+            //            break;
+            //        case "Glenn":
+            //            Console.WriteLine("Welcome, Glenn.");
+            //            authorizedUser = true;
+            //            break;
+            //    }
+            //}
+            do
+            {
+                switch (name)
+                {
+                    default:
+                        Console.WriteLine("You are not an authorized user.");
+                        Console.WriteLine("Please input a different name.");
+                        name = Console.ReadLine();
+                        break;
+                    case "Glenn":
+                        Console.WriteLine("Welcome, Glenn.");
+                        authorizedUser = true;
+                        break;
+                }
+            }
+            while (!authorizedUser);
+
+            Console.Read();
+        }
+    }
+}
