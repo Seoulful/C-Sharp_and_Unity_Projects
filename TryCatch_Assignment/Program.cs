325baf82083c0f2d4b12dbe46aada6e02f07b5d9
Seoulful <79722803+Seoulful@users.noreply.github.com>
2022-05-17 19:14:07 -0700

    added TryCatch_Assignment

---
+++ TryCatch_Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TryCatch_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Integers = new List<int>();
+            Integers.Add(10);
+            Integers.Add(20);
+            Integers.Add(40);
+            Integers.Add(80);
+            Integers.Add(160);
+
+            try
+            {
+                Console.WriteLine("Select a number to divide the integers by.");
+                int divideBy = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < Integers.Count; i++)
+                {
+                    int result = Integers[i] / divideBy;
+                    Console.WriteLine(result);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+            }
+
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't attempt to add by zero.");
+            }
+        }
+    }
+}
