0b61406b6dfe4853e4b11f275ae1a55fe688aae5
Seoulful <79722803+Seoulful@users.noreply.github.com>
2022-05-19 17:23:30 -0700

    added CallingMethods_Assignment

---
+++ CallingMethods_Assignment/Methods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods_Assignment
+{
+    public class Methods
+    {
+        public static void Method1(int input)
+        {
+            int result = input * 2;
+            Console.WriteLine("Your number multiplied by two is: " + result);
+        }
+
+        public static void Method2(int input)
+        {
+            int result = input + 3;
+            Console.WriteLine("Your number added by 3 is: " + result);
+        }
+
+        public static void Method3(int input)
+        {
+            int result = input / 2;
+            Console.WriteLine("Your number divided by 2 is: " + result);
+        }
+    }
+}
---
+++ CallingMethods_Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an integer.");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Methods.Method1(input);
+            Methods.Method2(input);
+            Methods.Method3(input);
+            Console.ReadLine();
+        }
+    }
+}
