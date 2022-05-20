993a2cd55bab4490b8416aef8f6ec20a14e627fb
Seoulful <79722803+Seoulful@users.noreply.github.com>
2022-05-20 16:10:22 -0700

    added MainMethod_Assignment

---
+++ MainMethod_Assignment/Method.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod_Assignment
+{
+    public class Method
+    {
+        public int mainMethod(int input)
+        {
+            return input * 2;
+        }
+
+        public decimal mainMethod(decimal input)
+        {
+            return input / 5;
+        }
+
+        public int mainMethod(string input)
+        {
+            int inputInt = Convert.ToInt32(input);
+            return inputInt + 3;
+        }
+    }
+}
---
+++ MainMethod_Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input1 = 10;
+            decimal input2 = 22.5m;
+            string input3 = "4";
+            var result = new Method();
+
+            Console.WriteLine("10 multiplied by 2 is: " + result.mainMethod(input1));
+            Console.WriteLine("22.5 divided by 5 is: " + result.mainMethod(input2));
+            Console.WriteLine("The string \"4\" converted to an integer, then added to 3 is: " + result.mainMethod(input3));
+            Console.ReadLine();
+        }
+    }
+}
