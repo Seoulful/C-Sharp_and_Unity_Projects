4a20b6caa2954af304e78e253c535337c4602291
Seoulful <79722803+Seoulful@users.noreply.github.com>
2022-05-20 18:09:54 -0700

    added MethodClass_Assignment

---
+++ MethodClass_Assignment/Method.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass_Assignment
+{
+    class Method
+    {
+        public void mainMethod(int input1, int input2)
+        {
+            int input1result = input1 * 2;
+            Console.WriteLine(input1result);
+            Console.WriteLine(input2);
+        }
+    }
+}
---
+++ MethodClass_Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an integer.");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input another integer.");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            var result = new Method();
+
+            result.mainMethod(input1: input1, input2: input2);
+            Console.ReadLine();
+        }
+    }
+}
