cb8e78ceb871b27a07a92af1f0485a65e836d2ba
Seoulful <79722803+Seoulful@users.noreply.github.com>
2022-05-20 18:01:08 -0700

    added MultipleParameterMethod_Assignment

---
+++ MultipleParameterMethod_Assignment/Method.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleParameterMethod_Assignment
+{
+    class Method
+    {
+        public int mainMethod(int input1, int input2 = 0)
+        {
+            return input1 + input2;
+        }
+    }
+}
---
+++ MultipleParameterMethod_Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleParameterMethod_Assignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var addTogether = new Method();
+            Console.WriteLine("Please input an integer.");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input another integer. If you'd like to omit this, please enter 0.");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your two integers added together are: " + addTogether.mainMethod(input1, input2));
+            Console.ReadLine();
+        }
+    }
+}
