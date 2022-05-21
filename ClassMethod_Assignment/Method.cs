cf182220eb9774ac625635644a506768cbf2d0c9
Seoulful <79722803+Seoulful@users.noreply.github.com>
2022-05-20 18:25:21 -0700

    added ClassMethods_Assignment

---
+++ ClassMethod_Assignment/Method.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod_Assignment
+{
+    class Method
+    {
+        public void mainMethod(int input)
+        {
+            int result = input / 2;
+            Console.WriteLine(result);
+        }
+
+        public static string mainMethod(string input, out string output)
+        {
+            input = "Hello, World!";
+            output = "Goodbye.";
+            return input;
+        }
+    }
+}
---
+++ ClassMethod_Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Method();
+            Console.WriteLine("Please input an integer.");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            result.mainMethod(input);
+            Console.ReadLine();
+        }
+    }
+}
