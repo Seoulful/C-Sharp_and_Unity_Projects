0d40ed2fc5ebae982e18006a8db6ff4c2aaa62de
Seoulful <79722803+Seoulful@users.noreply.github.com>
2022-05-25 18:17:33 -0700

    added ParsingEnums_Assignment

---
+++ ParsingEnums_Assignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums_Assignment
+{
+    class Program
+    {
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week.");
+
+                string userInput = Console.ReadLine();
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), userInput);
+
+                Console.WriteLine(day);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
